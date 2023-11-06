85f118d46bcf730635824445a36c070edbfcea25
torfirst <victoriah.first@gmail.com>
2023-11-05 18:01:22 -0600

    Updated and cleaned up code in index.html and script.js file, and filled out README file accordingly

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # work-day-scheduler
+
+## Description
+
+This project creates a work day scheduler for a typical 9-5 workday. This is great for users who need a current/focused view of their day. This is also ideal for those who like to lay out everything they need to do in the beginning of their work day, or for those who prep their schedule at the end of a shift for the next day. 
+
+## Installation
+
+Access the Work Day Scheduler [here](https://torfirst.github.io/work-day-scheduler/). A web browser is required to access the Work Day Scheduler. 
+
+## Usage
+
+Go to https://torfirst.github.io/work-day-scheduler/ to view the work day scheduler. To fill out information for a specific hour, simply click into the text field, type what you need, and hit the save button to the right of the text field. The text will remain when the page is refreshed. To clear the text, the user can go into the field they choose, delete everything inside the text area, and hit the save button on the right of the text field again. 
+
+The current hour will show in red, while everything in the past will show grey and everything in the future will show green.
+
+
+![This is what it would look like during the 5 o'clock hour.](images/Example.png)
+
+## Credits
+
+I used the starter code HTML and CSS as a basis, and updated the HTML accordingly.
+
+I also received guidance from the Xpert Learning Assistant located in BCS to remind me how to use the "this" function along with DOM traversal.
+
+I used [this link](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/split) to figure out how to split an ID and use the part I needed. I also referred to the [documentation for day.js](https://day.js.org/docs/en/installation/installation). 
+
+## License
+
+License in repo. 
--- script.js
+++ script.js
@@ -1,25 +1,18 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that the code isn't run until the browser has finished rendering all the elements in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should use the id in the containing time-block as a key to save the user input in local storage. HINT: What does `this` reference in the click listener function? How can DOM traversal be used to get the "hour-x" id of the time-block containing the button that was clicked? How might the id be useful when saving the description in local storage?
-
   // Attach click event listener to save button
   $('.save-button').on('click', function () {
-    // Get the parent time-block element and its id
+    // Get the parent time-block element and its ID
     var timeBlockId = $(this).closest('.time-block').attr('id');
 
     // Get the user input from the description input field
     var description = $(this).siblings('.description').val();
 
-    // Save the description in local storage using the time-block id as the key
+    // Save the description in local storage using the time-block ID as the key
     localStorage.setItem(timeBlockId, description);
   });
 
-  // TODO: Add code to apply the past, present, or future class to each time block by comparing the id to the current hour. HINTS: How can the id attribute of each time-block be used to conditionally add or remove the past, present, and future classes? How can Day.js be used to get the current hour in 24-hour time?
-  //
-
   let findHour = document.querySelectorAll(".time-block");
   const currentHour = dayjs().hour();
-  // console.log('currentHour: ', currentHour);
 
   findHour.forEach((div) => {
     const hourVal = div.id.split("-");
@@ -40,20 +33,15 @@ $(function () {
     }
   })
 
-
-  // TODO: Add code to get any user input that was saved in localStorage and set the values of the corresponding textarea elements. HINT: How can the id attribute of each time-block be used to do this?
-  //
-
   // Retrieve saved user input from localStorage and set textarea values
   for (let i = 9; i <= 17; i++) {
     const blockId = "hour-" + i;
-    const textareaId = "textarea-" + i;
-
     const savedInput = localStorage.getItem(blockId);
-    $("#" + textareaId).val(savedInput);
+    $("#" + blockId).children("textarea").val(savedInput)
   }
 
-  // TODO: Add code to display the current date in the header of the page.
   const currentDay = dayjs().format('MMMM DD, YYYY');
   $('#currentDay').text(currentDay);
 });
+
+
